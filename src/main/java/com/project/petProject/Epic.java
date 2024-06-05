ea6ebe5497fb76aaeee463420dec206b4f49292e
Tatiana Sergeeva <tanyushkadurneva15@gmail.com>
2024-06-05 22:15:24 +0300

    PR-26 создание Java объектов из YAML, сериализация объектов в JSON

---
+++ src/main/java/com/project/petProject/Epic.java
@@ -0,0 +1,36 @@
+package com.project.petProject;
+
+import java.util.List;
+
+public class Epic {
+    private String title;
+    private List<Task> taskList;
+
+    public Epic(String title, List<Task> taskList) {
+        this.title = title;
+        this.taskList = taskList;
+    }
+
+    public Epic(String title) {
+        this.title = title;
+    }
+
+    public Epic() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+}
